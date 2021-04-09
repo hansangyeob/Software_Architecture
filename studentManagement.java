5b9ed474bf8e9cad99f1bf6bcab8f5a51bc6b7d9
Tony Han <57244611+hansangyeob@users.noreply.github.com>
2021-04-09 15:45:53 +0700

    Delete studentManagement.java

--- studentManagement.java
+++
@@ -1,9 +0,0 @@
-package librarySystem;
-
-import java.util.ArrayList;
-
-public class studentManagement extends Student{
-
-    public static  ArrayList<Student> studentList = new ArrayList<>();
-
-}
